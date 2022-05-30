84a168eb279ea080c5698bbd55f93e9c3a9db85e
panmout <takismoutafis@outlook.com>
2022-05-30 22:37:46 +0300

    nothing

--- mbrCentroid.sh
+++ mbrCentroid.sh
@@ -1,11 +1,11 @@
 # execute (<namenode name> <hdfs dir name> <query dataset> <hdfs GNN dir name> <step> <mindist> <counter_limit> <pointdist>)
 
 hadoop jar ./target/hadoop-0.0.1-SNAPSHOT.jar gr.uth.ece.dsel.hadoop.preliminary.MbrCentroid \
-nameNode=panagiotis-lubuntu \
+nameNode=Hadoopmaster \
 queryDir=input \
-queryDataset=query-dataset.txt \
+queryDataset=linearwaterNNew_sub_2.8M.txt \
 gnnDir=gnn \
-step=0.000001 \
+step=0.00000001 \
 minDist=10 \
-counter_limit=100 \
-diff=0.000001
+counter_limit=500 \
+diff=0.00000001
